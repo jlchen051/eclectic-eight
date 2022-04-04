d95b991c0337b6955629113a5511a5c9eba47600
Vicky Yu <vickyyu@usc.edu>
2022-04-04 00:43:04 -0700

    fixed date issue

--- src/components/RouteURL.js
+++ src/components/RouteURL.js
@@ -10,7 +10,6 @@ import Schedule from './scheduler/Schedule';
 import SignIn from './login/Sign-in';
 import Register from './registration/Register';
 import RegisterationSuccess from './registration/RegistrationSuccess';
-import Homepage from './Homepage';
 
 let userToken = null;
 
